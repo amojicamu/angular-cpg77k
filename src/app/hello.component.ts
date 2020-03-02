ccc6255297c32b7648626c2276f752a63b0d697d
amojicamu <andres.mojica@quimera.co>
2020-03-02 18:45:23 +0100

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,11 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<button mat-raised-button color="primary" (click)="onClick()">Open Dialog</button>`,
+  template: `
+    <button mat-raised-button color="primary" (click)="onClick()">Open Dialog</button>
+    <br />
+    <br />
+    <a [routerLink]="[ '', {outlets: { modal: 'login' } } ]">Login</a>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
