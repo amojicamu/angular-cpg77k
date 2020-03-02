621ad928be4d3bc8d746fe749ff4acfe441a4c34
amojicamu <andres.mojica@quimera.co>
2020-03-02 19:39:11 +0100

    1 file modified

--- src/app/option-dialog.component.ts
+++ src/app/option-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { DialogComponent } from './dialog.component';
 
 @Component({
   selector: 'app-dialog-content',
@@ -6,8 +10,20 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
   <h1 mat-dialog-title>Options Dialog</h1>
   <div mat-dialog-content>
     <p> This is another dialog to prove we can display different ones<p>
-  </div>`,
+  </div>
+  <div mat-dialog-actions>
+    <button mat-button (click)="onNoClick()">No Thanks</button>
+  </div>
+  `,
   styles: []
 })
 export class OptionsDialogComponent  {
+   constructor(
+    private router : Router,
+    public dialogRef: MatDialogRef<DialogComponent>) {}
+
+  onNoClick(): void {
+    this.router.navigate([{outlets: {details: null}}]);
+    this.dialogRef.close();
+  }
 }
